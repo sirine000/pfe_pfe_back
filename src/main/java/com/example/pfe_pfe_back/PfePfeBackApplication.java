bdf6a1327e5b637a28554fe0b34c0ee8addae8be
sirine0000 <sirine.metoui@esprit.tn>
2024-08-18 23:27:42 +0000

    Premier commit

---
+++ src/main/java/com/example/pfe_pfe_back/PfePfeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfe_pfe_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfePfeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfePfeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pfe_pfe_back/PfePfeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfe_pfe_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfePfeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
